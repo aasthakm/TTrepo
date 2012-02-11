7e1e76a2d31b3adbf9e8b54987ee12cd964f19c0
Arpan Gujarati <arpan4444casino@gmail.com>
2012-02-11 23:01:11 +0530

    added TODO comments to main.c - Arpan

--- TimeTable/main.c
+++ TimeTable/main.c
@@ -3,15 +3,23 @@
 
 int main()
 {
-    printf("Hello"
-           " world!\n");
-    //char *file = "input_data/teachers.txt";
-    //char *file = "input_data/standards.txt";
-    //char *file = "input_data/subjects.txt";
-//    int size, retval;
-//    char **teacher_array;
-//    retval = parse_file(file, &teacher_array, &size);
-//    printf("retval = %d\n", retval);
-//    print_char_array(teacher_array, size);
+    printf("Hello world!\n");
+
+// TODO (@aastha) - try to implement these points
+
+// Get user input from the *.txt files
+// Just need to make respective calls here
+// Make sure all the comments are removed from the file
+// And we need to still write special functions for lectures.txt and periods.txt
+
+// Init the timte table structure based on the input values
+// Again just need to make a call, implementation is done
+
+// Start allocation
+// Lets first try to implement the algo without any kind of validation
+// This will be just the backtracking algo then trying all cases
+// We can try running it on small test cases
+// Once above feature is worknig well, we can introduce validations
+
     return 0;
 }
