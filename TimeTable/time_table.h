9306de4e330b0f78ecc5bb9d71719e0956281719
Arpan Gujarati <arpan4444casino@gmail.com>
2012-02-12 01:40:37 +0530

    commiting a manual merge

--- TimeTable/time_table.h
+++ TimeTable/time_table.h
@@ -17,10 +17,6 @@ extern void delete_time_table_per_teacher(lecture_t*** lecture, int i_index);
 extern void delete_day_time_table_per_teacher(lecture_t*** lecture, int i_index, int j_index);
 extern void delete_time_table(lecture_t*** time_table, int num_teachers,
                               int num_days, int num_periods);
-<<<<<<< HEAD
 extern int allocate(int teacher, int day, int period);
-=======
-extern void allocate(int teacher, int day, int period);
 
->>>>>>> f1f5bd29ea66336178ce1d30c5c67b4df813a1ec
 #endif //TIME_TABLE_H
