9ea52c5e58bb24fac89082bc1acf6997da3c1b37
Arpan Gujarati <arpan4444casino@gmail.com>
2012-03-23 20:51:14 +0530

    commit before push

---
+++ TimeTable/global.h
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+int counter1;
+int num_teachers;
+int num_sub;
+int num_days;
+int num_std;
+int num_periods;
+int num_teach_lecs;
+
+char **teacher_array;
+char **sub_array;
+char **days_array;
+char **std_array;
+
+#endif // GLOBAL_H
